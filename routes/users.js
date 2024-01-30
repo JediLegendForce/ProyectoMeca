21d3ff2656844e4bc2336216c5661a08650bb62c
Jonathan Maldonado <jmaldonado0095@gmail.com>
2024-01-29 19:32:35 -0600

    API Weather

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,22 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-
+var axios = require("axios");
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  res.send("WEATHER_API: " + process.env.WEATHER_API);
+});
+
+router.get("/getApi", async function (req, res, next) {
+  try {
+    const response = await axios.get(
+      "https://api.openweathermap.org/data/2.5/forecast?id=524901&appid=" +
+        process.env.WEATHER_API
+    );
+    res.send(response.data);
+  } catch (error) {
+    console.error("Error al hacer la solicitud con Axios:", error.message);
+    res.status(500).send("Error interno del servidor");
+  }
 });
 
 module.exports = router;
